4a1424bcd37363281761aba6e5a1df974bd972bb
stephanetatar <tatarstephane@me.com>
2020-01-07 14:54:52 +0100

    first commit regressionlinear

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, redirect, url_for, request, make_response, jsonify
+from sklearn.externals import joblib
+from flask_sqlalchemy import SQLAlchemy
+from models import Result
+from db import db, app
+
+import os
+import requests
+import json
+
+#app = Flask(__name__)
+
+#export APP_SETTINGS="config.DevelopmentConfig
+#app.config.from_object(os.environ["APP_SETTINGS"])
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    response = make_response(render_template("index.html"))
+    return response
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method=='POST':
+        regressor = joblib.load("./linear_regression_model.pkl")
+        data = dict(request.form.items())
+        years_of_experience = float(data["YearsExperience"])
+        prediction = regressor.predict([[years_of_experience]])
+
+        try:
+            response = make_response(render_template(
+            "predicted.html",
+            prediction = float(prediction)
+            ))
+        except ValueError as e:
+            print(e)
+
+            return jsonify("Please enter a number.")
+
+        result = Result(
+            YearsExperience=float(years_of_experience),
+            Prediction=float(prediction)
+        )
+        db.session.add(result)
+        db.session.commit()
+        print(db)
+
+        return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
